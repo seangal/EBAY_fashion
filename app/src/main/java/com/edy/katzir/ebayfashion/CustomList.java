adf7e31eb93d5190b035bebc0986900ce0e58f5c
JonatanR2 <jonatanr2@gmail.com>
2015-11-13 20:09:39 +0200

    added custom list

---
+++ app/src/main/java/com/edy/katzir/ebayfashion/CustomList.java
@@ -0,0 +1,43 @@
+package com.example.user.testapp;
+
+/**
+ * Created by USER on 13/11/2015.
+ */
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomList extends ArrayAdapter<String>{
+
+    private final Activity context;
+    private final String[] web;
+    private final Integer[] imageId;
+    public CustomList(Activity context,
+                      String[] web, Integer[] imageId) {
+        super(context, R.layout.list_single, web);
+        this.context = context;
+        this.web = web;
+        this.imageId = imageId;
+
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.list_single, null, true);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.txt);
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        txtTitle.setText(web[position] + " test");
+
+        imageView.setImageResource(imageId[position]);
+
+        return rowView;
+    }
+}
