6cfa685ce3927e55e59600991f842a62ed67053c
seangal <sean1815@gmail.com>
2015-11-13 17:41:35 +0200

    Ops

--- app/src/main/java/com/edy/katzir/ebayfashion/Measures.java
+++ app/src/main/java/com/edy/katzir/ebayfashion/Measures.java
@@ -1,17 +1,75 @@
 package com.edy.katzir.ebayfashion;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by sean1 on 13/11/2015.
  */
 public class Measures{
+    static TextView bust;
+    static TextView waste;
+    static TextView hips;
+    static SharedPreferences.Editor editor;
+    static Activity activity;
     static void onCreate(View view){
-        Spinner dropdown = (Spinner)view.findViewById(R.id.gender);
-        String[] items = new String[]{"Male", "Female"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown.setAdapter(adapter);
+        activity =(Activity)view.getContext();
+        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        bust=(TextView)view.findViewById(R.id.bust);
+        waste=(TextView)view.findViewById(R.id.waste);
+        hips=(TextView)view.findViewById(R.id.hips);
+        bust.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                editor.putString(activity.getString(R.string.bust), s.toString());
+                editor.commit();
+            }
+        });
+        waste.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                editor.putString(activity.getString(R.string.waste), s.toString());
+                editor.commit();
+            }
+        });
+        hips.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                editor.putString(activity.getString(R.string.hips), s.toString());
+                editor.commit();
+            }
+        });
     }
 }
--- app/src/main/java/com/edy/katzir/ebayfashion/Search.java
+++ app/src/main/java/com/edy/katzir/ebayfashion/Search.java
@@ -12,63 +12,6 @@ import android.widget.TextView;
  * Created by sean1 on 13/11/2015.
  */
 public class Search {
-    static TextView bust;
-    static TextView waste;
-    static TextView hips;
-    static SharedPreferences.Editor editor;
-    static Activity activity;
     static void onCreate(View view){
-        activity =(Activity)view.getContext();
-        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
-        editor = sharedPref.edit();
-        bust=(TextView)view.findViewById(R.id.bust);
-        waste=(TextView)view.findViewById(R.id.waste);
-        hips=(TextView)view.findViewById(R.id.hips);
-        bust.setText();
-        bust.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                editor.putString(activity.getString(R.string.bust), s.toString());
-                editor.commit();
-            }
-        });
-        waste.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                editor.putString(activity.getString(R.string.waste), s.toString());
-                editor.commit();
-            }
-        });
-        hips.addTextChangedListener(new TextWatcher() {
-
-            public void afterTextChanged(Editable s) {
-            }
-
-            public void beforeTextChanged(CharSequence s, int start,
-                                          int count, int after) {
-            }
-
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
-                editor.putString(activity.getString(R.string.hips), s.toString());
-                editor.commit();
-            }
-        });
     }
 }
